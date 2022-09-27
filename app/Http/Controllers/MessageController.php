d7917b2ea9c8ec289078b299d5f4e6298dfc6d31
Sardor Akhmadjonov <78022037+SardorAkh@users.noreply.github.com>
2022-09-27 19:11:26 +0500

    MessageControllers

---
+++ app/Http/Controllers/MessageController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Message;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use function PHPUnit\Framework\returnArgument;
+
+class MessageController extends Controller
+{
+    /**
+     * Retrieves message list which depends on last message
+     *
+     * @param  Request $request
+     * @return JsonResponse
+     */
+    public function getMessages(Request $request): JsonResponse
+    {
+        $query = $request->query;
+        if($query->count() == 0)
+            return response()->json(Message::all());
+        $data = Message::where("id",">",$query->get("id"))->get();
+        return response()->json($data);
+    }
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function store(Request $request): JsonResponse
+    {
+
+        $user_id = $request->json()->get("user_id");
+        $content = $request->json()->get("content");
+
+        Message::create([
+           "content" => $content,
+           "user_id" => $user_id
+        ]);
+
+        return response()->json("OK");
+    }
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Message  $message
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Message $message)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @param  \App\Models\Message  $message
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Message $message)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Message  $message
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Message $message)
+    {
+        //
+    }
+}
